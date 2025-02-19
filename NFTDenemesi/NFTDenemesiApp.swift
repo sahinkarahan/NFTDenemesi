7d61bb3c58458cc745273819f670815598dcf8cc
sahinkarahan <sahin.karahan@hotmail.com>
2025-02-19 15:26:37 +0300

    Initial Commit

---
+++ NFTDenemesi/NFTDenemesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTDenemesiApp.swift
+//  NFTDenemesi
+//
+//  Created by Şahin Karahan on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct NFTDenemesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
